605dc57b8402f21cc7c7ccc2647f85bbfa177489
Ranim Taboush <ranim.taboush@gmail.com>
2024-06-30 18:05:27 +0300

    domain

--- src/config/api.js
+++ src/config/api.js
@@ -2,7 +2,7 @@ export const ApiBase = "https://arabhardware.com/api/v1"
 export const SetOpenCart = "https://stg22x.promise.com.sa/index.php?route=extension/mstore/account/login"
 
 // export const thisDomain = "localhost"
-export const thisDomain = "user-profile-lyart.vercel.app"
+export const thisDomain = "app.vercel.user-profile-lyart"
 
 let res={
 	"status": "success",
