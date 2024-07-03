81766e8c477be883d769b1cfb1025afdf104ba49
Ranim Taboush <ranim.taboush@gmail.com>
2024-07-03 13:32:37 +0300

    .net

--- src/app/user/page.js
+++ src/app/user/page.js
@@ -1,24 +1,24 @@
 import React from 'react'
 import { getSession } from "next-auth/react"
 
-export async function getServerSideProps(context) {
-    const session = await getSession(context)
+// export async function getServerSideProps(context) {
+//     const session = await getSession(context)
   
-    if (!session) {
-      return {
-        redirect: {
-          destination: "/login",
-          permanent: false
-        }
-      }
-    }
+//     if (!session) {
+//       return {
+//         redirect: {
+//           destination: "/login",
+//           permanent: false
+//         }
+//       }
+//     }
   
-    return {
-      props: {
-        user: session.user
-      }
-    }
-  }
+//     return {
+//       props: {
+//         user: session.user
+//       }
+//     }
+//   }
 
 function Page({user}) {
   return (
