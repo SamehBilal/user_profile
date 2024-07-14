7fbf57bdc94ff45317964144268554ccc36d1e8b
Ranim Taboush <ranim.taboush@gmail.com>
2024-07-14 09:17:44 +0300

    uninstall

--- src/app/social/logout/searchParamsComponent.jsx
+++ src/app/social/logout/searchParamsComponent.jsx
@@ -1,14 +1,11 @@
 
 import { useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
-import { useCookies } from 'next-client-cookies';
 import { deleteCookie, getCookie } from 'cookies-next';
 import { thisDomain } from '@/config/api'
 
 export default function SearchParamsComponent({}) {
     const searchParams = useSearchParams()
-    const cookies = useCookies();
-
     useEffect(()=>{
       const token = getCookie('token')
       const user = getCookie('user')
