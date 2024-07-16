cde6959d0333548be7f52f6fe78252e08c6e7014
Ranim Taboush <ranim.taboush@gmail.com>
2024-07-16 18:57:00 +0300

    12000

--- src/app/login_callback/page.jsx
+++ src/app/login_callback/page.jsx
@@ -37,7 +37,7 @@ function Page() {
         // console.log('returnUrl', returnUrl)
         // console.log('to', `${returnUrl}${(sessionId && returnUrl.includes('ahw.store')) ?`&session_id=${sessionId}`:''}`)
           location.href = `${returnUrl}${(sessionId && returnUrl.includes('ahw.store')) ?`&session_id=${sessionId}`:''}`
-      }, 3000);
+      }, 12000);
       return ()=>{
           clearTimeout(timer)
       }
