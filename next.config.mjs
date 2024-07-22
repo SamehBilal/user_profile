e8037625bdd4fc2258c80c8091b12ff6c57ae977
Ranim Taboush <ranim.taboush@gmail.com>
2024-07-22 15:28:45 +0300

    *

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,20 @@ const nextConfig = {
     async headers() {
         return [
             {
+                source: "/:path*",
+                headers: [
+                    { key: 'Content-Security-Policy',  value: "frame-ancestors 'self' *;" },
+                    { key: 'X-Frame-Options', value: 'allow-from *', },
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" },
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
+                ]
+            },
+            {
                 source: "/(.*)?",
                 headers: [
+                    { key: 'Content-Security-Policy',  value: "frame-ancestors 'self' https://sso-1.vercel.app;" },
                     { key: 'X-Frame-Options', value: 'allow-from *', },
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
