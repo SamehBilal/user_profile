aba03452464fe8359f95d9bd881cc5b1beed28ed
Ranim Taboush <ranim.taboush@gmail.com>
2024-07-22 15:20:52 +0300

    allow from

--- next.config.mjs
+++ next.config.mjs
@@ -4,17 +4,9 @@ const nextConfig = {
     async headers() {
         return [
             {
-                source: "/api/refresh",
+                source: "/(.*)?",
                 headers: [
-                    { key: "Access-Control-Allow-Credentials", value: "false" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
-                ]
-            },
-            {
-                source: "/:path*",
-                headers: [
+                    { key: 'X-Frame-Options', value: 'allow-from *', },
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" },
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
