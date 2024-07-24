45b6487cbd6ade6d2ebee52ac4828e3a89f56132
Ranim Taboush <ranim.taboush@gmail.com>
2024-07-24 14:18:56 +0300

    inovice

--- src/app/invoice/page.jsx
+++ src/app/invoice/page.jsx
@@ -1,113 +1,118 @@
 import React from 'react'
 import Image from 'next/image'
+import InvoiceBg from '@/public/images/backgrounds/invoice-bg.png'
+import Logo from '@/public/images/logos/ahw_logo.svg'
+import QR from '@/public/images/qr.png'
 import Appbar from '@/components/appbar'
+import { MapPin, Phone, Mail } from 'lucide-react'
 
 function Page() {
+    const data = {
+        headers: ['رقم', 'وصف المنتج', 'السعر', 'الكمية', 'المجموع'],
+        rows: [
+            ['لوريم ايبسوم', '$10', '1', '$10'],
+            ['لوريم ايبسوم', '$20', '1', '$20'],
+            ['لوريم ايبسوم', '$30', '1', '$30'],
+            ['لوريم ايبسوم', '$40', '1', '$40'],
+            ['لوريم ايبسوم', '$50', '1', '$50'],
+            ['لوريم ايبسوم', '$50', '1', '$50'],
+        ]
+    }
+
   return (
-    <div className=" max-w-3xl my-12 mx-auto bg-white dark:text-black p-5 shadow-sm">
-        <header className="text-center">
-            <h1 className=' text-2xl'>Payment Invoice</h1>
-            <p className='my-1 text-sm'>No. KLLM 233-000023</p>
-            <div className="flex items-center justify-center mt-2">
-                <img src="./images/logo.png" alt="Cosmetics Logo" />
-                <h2>COSMETICS</h2>
+    <div className=" max-w-3xl my-12 mx-auto bg-white text-black shadow-sm relative">
+        <Image src={InvoiceBg} alt='retro environment' className='size-full object-cover' />
+        <div className="absolute inset-0 z-10 p-10 space-y-6">
+            <header className="text-center flex items-center justify-between">
+                <div className="space-y-1">
+                    <h1 className=' text-2xl font-bold'>تفاصيل الدفع</h1>
+                    <p className='my-1 text-sm'>شارع رقم: 233-000023</p>
+                </div>
+                <div className="">
+                    <Image src={Logo} alt="Arabhardware" className='size-12 object-center mx-auto' />
+                    <h2 className='text-center'>عرب هاردوير</h2>
+                </div>
+            </header>
+            <section className="flex justify-between items-end">
+                <div className='w-[45%] space-y-1'>
+                    <p className='font-bold'>فاتورة إلى</p>
+                    <h3 className='text-primary'>حسن حسنين</h3>
+                    <p>30 جامعة الدول، القاهرة</p>
+                    <p>(020)34444333</p>
+                </div>
+                <div className='w-[45%] space-y-1'>
+                    <p>
+                        <span className='font-bold'>رقم الحساب: </span>
+                        <span>2222 1111 3333 4444</span>
+                    </p>
+                    <p>
+                        <span className='font-bold'>التاريخ: </span>
+                        <span>23-01-2024</span>
+                    </p>
+                </div>
+            </section>
+            <table className="w-full border-separate">
+                <thead>
+                    <tr className='[&>*]:border [&>*]:border-solid [&>*]:border-[#ddd] [&>*]:bg-[#f2f2f2] [&>*]:p-2 [&>*]:text-center'>
+                        {data.headers.map((_, i)=>{
+                            return <th key={i} className=''>{_}</th>
+                        })}
+                    </tr>
+                </thead>
+                <tbody>
+                    {/* <th className=''>{`${index}.`}</th> */}
+                    {data.rows.map((row, index)=>{
+                        return <tr key={index} 
+                        className='[&>*]:border [&>*]:border-solid [&>*]:border-[#ddd] [&>*]:p-2 [&>*]:text-center'>
+                            <td className=''>{`${index+1}.`}</td>
+                            {row.map((_, i)=>{
+                                return <td key={i} className=''>{_}</td>
+                            })}
+                        </tr>
+                    })}
+                </tbody>
+            </table>
+            <div className="text-right space-y-1 w-52">
+                <p className="flex items-center justify-between">
+                    <span>المجموع الجزئي: </span>
+                    <span>210$</span>
+                </p>
+                <p className="flex items-center justify-between">
+                    <span>الضريبة: </span>
+                    <span>0.0%</span>
+                </p>
+                <p className="flex items-center justify-between">
+                    <span>المجموع: </span>
+                    <span>210$</span>
+                </p>
             </div>
-        </header>
-        <section className="flex justify-between my-5">
-            <div className='w-[45%] space-y-1'>
-                <p>Invoice to:</p>
-                <h3>Jarod Brown</h3>
-                <p>30th Street, Side Lake City</p>
-                <p>(3445) 3444 4333</p>
-            </div>
-            <div className='w-[45%] space-y-1'>
-                <p>Account No.: 0000 12345 67890</p>
-                <p>Date: January 23, 2023</p>
-            </div>
-        </section>
-        <table className="w-full border-collapse my-5">
-            <thead>
-                <tr>
-                    <th className='border border-[#ddd] border-solid p-2 text-center bg-[#f2f2f2]'>No</th>
-                    <th className='border border-[#ddd] border-solid p-2 text-center bg-[#f2f2f2]'>Item Description</th>
-                    <th className='border border-[#ddd] border-solid p-2 text-center bg-[#f2f2f2]'>Price</th>
-                    <th className='border border-[#ddd] border-solid p-2 text-center bg-[#f2f2f2]'>Qty.</th>
-                    <th className='border border-[#ddd] border-solid p-2 text-center bg-[#f2f2f2]'>Total</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>1.</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>Lorem ipsum dolor</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>$10</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>1</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>$10</td>
-                </tr>
-                <tr>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>2.</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>Lorem ipsum dolor</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>$20</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>1</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>$20</td>
-                </tr>
-                <tr>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>3.</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>Lorem ipsum dolor</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>$30</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>1</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>$30</td>
-                </tr>
-                <tr>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>4.</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>Lorem ipsum dolor</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>$40</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>1</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>$40</td>
-                </tr>
-                <tr>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>5.</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>Lorem ipsum dolor</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>$50</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>1</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>$50</td>
-                </tr>
-                <tr>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>6.</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>Lorem ipsum dolor</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>$60</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>1</td>
-                    <td className='border border-[#ddd] border-solid p-2 text-center'>$60</td>
-                </tr>
-            </tbody>
-        </table>
-        <div className="text-right my-5 space-y-1">
-            <p>Sub Total: $210</p>
-            <p>Tax: 0.0%</p>
-            <p>Total: $210</p>
+            <section className="flex justify-between">
+                <div className="w-[60%]">
+                    <h4>الشروط والاحكام</h4>
+                    <p>
+                    طراف الأرضية والنرويج أي دول. أمّا إيطاليا بمعارضة و غير, ما الى أراضي اعتداء ويكيبيديا. السبب الأرضية اليابان أما ما, يرتبط وقوعها، تكتيكاً وفي مع. اعتداء وصافرات والكساد كل بحق. الا اكتوبر الطرفين من, أن حيث بفرض الأوربيين.
+                    </p>
+                </div>
+                <div className="w-[35%] text-center">
+                    <Image src={QR} alt="QR Code" className='w-20 my-2 mx-auto' />
+                    <p>رمز التتبع</p>
+                </div>
+            </section>
+            <footer className="text-center space-y-1 text-[#333] pt-10">
+                <div className="flex items-center justify-start gap-2">
+                    <MapPin className='bg-black rounded-lg p-1 size-6 text-white' strokeWidth={2} />
+                    <p className=''>130th Street, Sky View</p>
+                </div>
+                <div className="flex items-center justify-start gap-2">
+                    <Phone className='bg-black rounded-lg p-1 size-6 text-white' strokeWidth={2} />
+                    <p className=''>(2332) 23332 3233</p>
+                </div>
+                <div className="flex items-center justify-start gap-2">
+                    <Mail className='bg-black rounded-lg p-1 size-6 text-white' strokeWidth={2} />
+                    <a href="mailto:youremail@example.com" className=''>youremail@example.com</a>
+                </div>
+            </footer>
         </div>
-        <section className="fle justify-between my-5">
-            <div className="w-[60%]">
-                <h4>Terms & Condition</h4>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna aliquam erat volutpat.</p>
-            </div>
-            <div className="w-[35%] text-center">
-                <p>Scan here for payment</p>
-                <img src="./images/qrcode.png" alt="QR Code" className='w-full my-2' />
-                <p>Scan to track</p>
-            </div>
-        </section>
-        <footer className="text-center my-5">
-            <p>Susan Kamel</p>
-            <h4>Susan Kamelia</h4>
-            <p>Branch Manager</p>
-        </footer>
-        <address className="text-center my-5 space-y-1">
-            <p className='text-[#333]'>130th Street, Sky View</p>
-            <p className='text-[#333]'>(2332) 23332 3233</p>
-            <p className='text-[#333]'>
-                <a href="mailto:youremail@example.com" className=''>youremail@example.com</a>
-            </p>
-        </address>
     </div>
   )
 }
