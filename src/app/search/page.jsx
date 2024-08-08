5d72bc9daee4dfad2d4042442db97c8e47df650e
Ranim Taboush <ranim.taboush@gmail.com>
2024-08-08 09:23:04 +0300

    footer

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,5 +1,6 @@
 "use client"
 import AppBar from "@/components/appbar";
+import Footer from "@/components/footer";
 import SearchPage from "@/components/search_page";
 import { searchData, statusData } from "@/components/search_page/data";
 import { useState } from "react";
@@ -8,7 +9,7 @@ export default function Psge({}) {
   const [bgImg, setBgImg] = useState(searchData[0].backgroundImg)
   
   return (
-    <main className="py-12 w-full min-h-screen relative">
+    <main className="pt-12 w-full min-h-screen relative overflow-hidden">
       
       <div className="absolute inset-0 w-full h-[70vh] bg-cover bg-no-repeat"
         style={{
@@ -21,6 +22,7 @@ export default function Psge({}) {
 
       <AppBar shadow='transparent' bgTransparent={false} />
       <SearchPage data={searchData} setBgImg={setBgImg} statusData={statusData} />
+      <Footer />
     </main>
   );
 }
