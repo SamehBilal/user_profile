675fbe134332b787ce6a400ba3cf7d6e221a14d1
Ranim Taboush <ranim.taboush@gmail.com>
2024-08-08 15:40:24 +0300

    yes

--- src/app/api/refresh/route.js
+++ src/app/api/refresh/route.js
@@ -7,14 +7,14 @@ export async function GET(request, response) {
 
     try{
         const requestHeaders = new Headers(request.headers)
-        headers.set("Access-Control-Allow-Origin", "*");
-        headers.set("Content-Security-Policy", "frame-ancestors 'self' *");
-        headers.set("Age", "3000");
-        headers.set("Cache-Control", "max-age=3000");
-        headers.set("X-Frame-Options", "ALLOW-FROM *");
-        headers.set("Access-Control-Allow-Credentials", "true");
-        headers.set("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT");
-        headers.set("Access-Control-Allow-Headers", "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date");
+        // headers.set("Access-Control-Allow-Origin", "*");
+        // headers.set("Content-Security-Policy", "frame-ancestors 'self' *");
+        // headers.set("Age", "3000");
+        // headers.set("Cache-Control", "max-age=3000");
+        // headers.set("X-Frame-Options", "ALLOW-FROM *");
+        // headers.set("Access-Control-Allow-Credentials", "true");
+        // headers.set("Access-Control-Allow-Methods", "GET,DELETE,PATCH,POST,PUT");
+        // headers.set("Access-Control-Allow-Headers", "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date");
 
 
 
