ae2fde84622a605e895879d44171119d09f2b624
Ranim Taboush <ranim.taboush@gmail.com>
2024-08-08 15:22:28 +0300

    trying vercel.json

--- next.config.mjs
+++ next.config.mjs
@@ -10,29 +10,29 @@ const nextConfig = {
     // reactStrictMode: true,
     async headers() {
         return [
-            {
-                source: "/:path*",
-                headers: [
-                    { key: 'Content-Security-Policy',  value: "frame-ancestors 'self' *" },
-                    { key: 'X-Frame-Options', value: 'allow-from *', },
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Age", value: "5000"},
-                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
-                ]
-            },
-            {
-                source: "/refresh/cookie",
-                headers: [
-                    { key: 'Content-Security-Policy',  value: "frame-ancestors 'self' *" },
-                    { key: 'X-Frame-Options', value: 'allow-from *', },
-                    { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "*" },
-                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
-                    { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
-                ]
-            }
+            // {
+            //     source: "/:path*",
+            //     headers: [
+            //         { key: 'Content-Security-Policy',  value: "frame-ancestors 'self' *" },
+            //         { key: 'X-Frame-Options', value: 'allow-from *', },
+            //         { key: "Access-Control-Allow-Credentials", value: "true" },
+            //         { key: "Access-Control-Allow-Origin", value: "*" },
+            //         { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+            //         { key: "Age", value: "5000"},
+            //         { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
+            //     ]
+            // },
+            // {
+            //     source: "/refresh/cookie",
+            //     headers: [
+            //         { key: 'Content-Security-Policy',  value: "frame-ancestors 'self' *" },
+            //         { key: 'X-Frame-Options', value: 'allow-from *', },
+            //         { key: "Access-Control-Allow-Credentials", value: "true" },
+            //         { key: "Access-Control-Allow-Origin", value: "*" },
+            //         { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+            //         { key: "Access-Control-Allow-Headers", value: "Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date" },
+            //     ]
+            // }
         ]
     },
     images: {
