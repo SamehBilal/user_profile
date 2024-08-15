60ca936b468582bdb5a444834d0db40e607a74cf
Ranim Taboush <ranim.taboush@gmail.com>
2024-08-15 15:07:58 +0300

    fix error

--- src/components/search_page/cardsComponent.jsx
+++ src/components/search_page/cardsComponent.jsx
@@ -3,9 +3,8 @@ import SingleCard from "./singleCard";
 import { useState, useEffect } from "react";
 import { Link } from "@nextui-org/react";
 
-export default function CardsComponent({cards=[], id='blogs'}) {
-  const [cardTypeIndex, setCardTypeIndex] = useState({blogs: 0, products: 1, vedios: 2, reviews: 3})
-  const [cardTypeLastIndex, setCardTypeLastIndex] = useState({blogs: 0, products: 1, vedios: 2, reviews: 3})
+
+const CardComponent = ({i=0, id, card, cardTypeLastIndex, cardTypeIndex}) => {
   const titles = {
     blogs:  "أحدث المقالات", 
     products: "أحدث المنتجات", 
@@ -31,6 +30,42 @@ export default function CardsComponent({cards=[], id='blogs'}) {
     reviews: 5
   }
 
+  const getIndex = (type, i) => {
+    const theIndex = i - cardTypeIndex[type]
+    return theIndex
+  }
+
+  return <>
+  {i==cardTypeIndex[card.type] && 
+  <div className="col-span-6 h-14 relative">
+    <p className="absolute inset-0 font-bold text-3xl top-2/3 -translate-y-1/2">
+      {titles[card.type]}
+    </p>
+  </div>}
+
+  {(id!='all' || getIndex(card.type, i)<maxLengthPreviewAll[card.type]) && //في الصفحة الرئيسية، رندر بس اللعدد المحدد
+  <SingleCard key={i} index={id!='all'?i:getIndex(card.type, i)} 
+  type={card.type} title={card.title} subTitle={card.subTitle} imgUrl={card.imgUrl}
+  desc={card.desc} price={card.price} inStock={card.inStock} url={card.url} youtubeId={card.youtubeId} />}
+
+  {i==cardTypeLastIndex[card.type]&& 
+  <div className="col-span-6 h-8 relative w-full">
+      <Link
+      className="absolute left-0 font-bold text-small top-1/3 -translate-y-1/2 w-fit"
+      isExternal
+      showAnchorIcon
+      href={links[card.type]}
+    >
+      {toLinks[card.type]}
+    </Link>
+  </div>}
+  </>
+}
+
+export default function CardsComponent({cards=[], id='blogs'}) {
+  const [cardTypeIndex, setCardTypeIndex] = useState({blogs: 0, products: 1, vedios: 2, reviews: 3})
+  const [cardTypeLastIndex, setCardTypeLastIndex] = useState({blogs: 0, products: 1, vedios: 2, reviews: 3})
+
   useEffect(()=>{
     const getEachCardTypeIndex = () => {
       const blogIndex = cards.findIndex(card => card.type === 'blogs');
@@ -49,40 +84,12 @@ export default function CardsComponent({cards=[], id='blogs'}) {
     }
     getEachCardTypeIndex()
   }, [])
-
-  const getIndex = (type, i) => {
-    const theIndex = i - cardTypeIndex[type]
-    return theIndex
-  }
   
   return (
     <div className={`max-w-grid grid grid-cols-6 gap-4 items-center justify-between`}>
       {cards.map((card, i)=>{
-        return <>
-        {i==cardTypeIndex[card.type] && 
-        <div className="col-span-6 h-14 relative">
-          <p className="absolute inset-0 font-bold text-3xl top-2/3 -translate-y-1/2">
-            {titles[card.type]}
-          </p>
-        </div>}
-
-        {(id!='all' || getIndex(card.type, i)<maxLengthPreviewAll[card.type]) && //في الصفحة الرئيسية، رندر بس اللعدد المحدد
-        <SingleCard key={i} index={id!='all'?i:getIndex(card.type, i)} 
-        type={card.type} title={card.title} subTitle={card.subTitle} imgUrl={card.imgUrl}
-        desc={card.desc} price={card.price} inStock={card.inStock} url={card.url} youtubeId={card.youtubeId} />}
-
-        {i==cardTypeLastIndex[card.type]&& 
-        <div className="col-span-6 h-8 relative w-full">
-            <Link
-            className="absolute left-0 font-bold text-small top-1/3 -translate-y-1/2 w-fit"
-            isExternal
-            showAnchorIcon
-            href={links[card.type]}
-          >
-            {toLinks[card.type]}
-          </Link>
-        </div>}
-        </>
+        return <CardComponent key={i} i={i} card={card} cardTypeLastIndex={cardTypeLastIndex}
+        cardTypeIndex={cardTypeIndex} id={id} />
       })}
     </div>
   );
