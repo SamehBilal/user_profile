215e2007ee5d10ad732f813e9f1794f4d1e6a854
Ranim Taboush <ranim.taboush@gmail.com>
2024-08-22 18:06:09 +0300

    update on the appbar

--- src/components/appbar/navbar_top_menu.jsx
+++ src/components/appbar/navbar_top_menu.jsx
@@ -8,7 +8,7 @@ import MorningList from './morning_list'
 
 function NavbarTopMenu({setTheme, setMoreDropdownPopoverShow, theme, setUserDropdownPopoverShow, forMobile=false}) {
   return (
-    <div className={`flex items-center ${forMobile?'justify-between mb-10':'justify-end'} gap-8 text-black dark:text-white`}>
+    <div className={`flex items-center ${forMobile?'justify-between mb-10':'justify-between'} gap-8 text-black dark:text-white`}>
         {theme=='dark' 
         ?<MoonIcon strokeWidth={2} onClick={() => setTheme('light')}
         className='size-8 cursor-pointer' />
@@ -17,10 +17,12 @@ function NavbarTopMenu({setTheme, setMoreDropdownPopoverShow, theme, setUserDrop
         <LayoutGrid id='more_dropdown_trigger' strokeWidth={2} onClick={()=>setMoreDropdownPopoverShow(prev=>!prev)}
         className='size-8 cursor-pointer' />
         {/* <Bell  strokeWidth={2} className='size-8 cursor-pointer' /> */}
-        <MorningList />
-        {/* <p className="">{ar.navbar.greeting}</p> */}
-        <Image src={PersonImg} alt='user image' className=' size-10 rounded-lg block cursor-pointer'
-        onClick={()=>setUserDropdownPopoverShow(prev=>!prev)} />
+        <div className="flex items-center gap-2">
+          {/* <p className="mr-4">{ar.navbar.greeting}</p> */}
+          <MorningList />
+          <Image src={PersonImg} alt='user image' className=' size-12 rounded-lg block cursor-pointer border border-primary border-solid'
+          onClick={()=>setUserDropdownPopoverShow(prev=>!prev)} />
+        </div>
     </div>
   )
 }
